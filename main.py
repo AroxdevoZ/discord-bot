584a4e90251039fbd723e4d8e6bae93dbc423094
AroxdevoZ <113457380+AroxdevoZ@users.noreply.github.com>
2022-09-14 18:22:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -51,6 +51,6 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
 
-bot.login(process.env.TOKEN)
+bot.run(heroku config:unset TOKEN)
 
 
