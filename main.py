526ebfd113274560409e60fe4b7773edfbbd5c94
AroxdevoZ <113457380+AroxdevoZ@users.noreply.github.com>
2022-09-14 18:13:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -51,6 +51,6 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
 
-bot.run(os.getenv("process.env.TOKEN"))
+bot.run(process.env.TOKEN)
 
 
