b9b4ba7215c5da1a3fcec545b6effae5e1194e04
AroxdevoZ <113457380+AroxdevoZ@users.noreply.github.com>
2022-09-16 18:31:14 +0200

    Update CommandesStaff.py

--- cogs/CommandesStaff.py
+++ cogs/CommandesStaff.py
@@ -250,9 +250,7 @@ class CommandesStaff(commands.Cog):
     async def getMemberRole(self, ctx):
         roles = ctx.guild.roles
         for role in roles:
-            if role.name == "Matelot":
-                return role
-            elif role.name == "Atlas":
+            if role.name == "Matelot" or "Atlas":
                 return role
 
         return await self.createMemberRole(ctx)
