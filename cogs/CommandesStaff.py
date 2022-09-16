4a2f3d94b4f42e8b5a039357483c390689a7ab9a
AroxdevoZ <113457380+AroxdevoZ@users.noreply.github.com>
2022-09-16 18:43:24 +0200

    Update CommandesStaff.py

--- cogs/CommandesStaff.py
+++ cogs/CommandesStaff.py
@@ -249,11 +249,9 @@ class CommandesStaff(commands.Cog):
 
     async def getMemberRole(self, ctx):
         roles = ctx.guild.roles
-        for role in roles:
-            if role.name == "Matelot":
-                return role
-            elif role.name == "Atlas":
-                return role
+        #for role in roles:
+            #if role.name == "Matelot":
+                #return role
         return await self.createMemberRole(ctx)
 
     # Commande pour Mute un membre
